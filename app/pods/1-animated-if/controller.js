334fde6d39c140a68f3047ae77de70c15a940774
Webb Henderson <webb.henderson@gmail.com>
2020-08-07 13:19:40 -0600

    Add AnimatedContainer to animated-if example

--- app/pods/1-animated-if/controller.js
+++ app/pods/1-animated-if/controller.js
@@ -1,6 +1,6 @@
 import Controller from "@ember/controller";
 import fade from "ember-animated/transitions/fade";
 
-export default Controller.extend({
-  transition: fade,
-});
+export default class AnimatedIfController extends Controller {
+  transition = fade;
+}
